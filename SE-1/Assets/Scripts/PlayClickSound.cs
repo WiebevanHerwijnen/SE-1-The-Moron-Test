e1a7ab81db5d65514ef1458b9b9c3b371590825d
MortalisVictrix <rjuithol@hotmail.nl>
2017-10-26 15:40:50 +0200

    Edit: PlaySound

--- SE-1/Assets/Scripts/PlayClickSound.cs
+++ SE-1/Assets/Scripts/PlayClickSound.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayClickSound : MonoBehaviour {
 
+    public AudioClip gneurshk;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        this.gameObject.AddComponent<AudioSource>();
+        this.GetComponent<AudioSource>().clip = gneurshk;
+        //GetComponent<AudioSource>().Play;
 	}
 	
 	// Update is called once per frame
