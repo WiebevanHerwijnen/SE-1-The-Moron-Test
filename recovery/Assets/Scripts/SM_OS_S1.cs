ef7fb752a387967adf325e02a486ebfdb27476e3
MortalisVictrix <rjuithol@hotmail.nl>
2017-11-01 16:20:04 +0100

    Fixed SceneManager

---
+++ recovery/Assets/Scripts/SM_OS_S1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Library voor SceneManagement
+
+public class SM_OS_S1 : MonoBehaviour {
+
+    public void MissionCompleted()
+    {
+        Debug.Log("Invisible Button is clicked"); // print of de Invisible Button gebruikt wordt
+
+        SceneManager.LoadScene("Main"); // Laad de scene Main in
+    }
+
+}
