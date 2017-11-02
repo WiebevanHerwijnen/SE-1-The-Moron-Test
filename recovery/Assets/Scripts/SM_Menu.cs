fd16451efe8f9c80fcd2b38ca203c44be9d26670
MortalisVictrix <rjuithol@hotmail.nl>
2017-11-02 18:56:39 +0100

    Added Scripts for functioning Menu

---
+++ recovery/Assets/Scripts/SM_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SM_Menu : MonoBehaviour {
+
+    public void MenuOpenMoronTest()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void MenuContinueMoronTest()
+    {
+
+    }
+}
